33a4125489723d6c5072147f58d22c7e4186257c
Ayyuce Demirbas <ayyucedemirbas@windowslive.com>
2020-04-26 02:35:36 +0300

    prediction
    prediction calculator method

--- src/kNN/kNearestNeighbor.java
+++ src/kNN/kNearestNeighbor.java
@@ -84,14 +84,66 @@ public class kNearestNeighbor {
 
  		}
      }
+     static void writeFrequencyToFile(String[] arr) throws FileNotFoundException {
+    	 PrintWriter p = new PrintWriter("/home/ayyuce/Desktop/set.txt");
+     	List asList = Arrays.asList(arr);
+  		Set<String> mySet = new HashSet<String>(asList);
+  		for(String s: mySet){
+          
+  		 p.println(s + " " +Collections.frequency(asList,s));
+          
+  		}
+  		p.close();
+      }
 	
      static void kNN(double _x, double _y) throws FileNotFoundException {
     	 putToHashMap(_x , _y);
      }
      
+     static void prediction(String[] arr, int k) throws FileNotFoundException {
+    	 writeFrequencyToFile(arr);
+    	 File f = new File("/home/ayyuce/Desktop/set.txt"); 
+    	 Scanner sc= new Scanner(f);
+    	 Scanner s= new Scanner(f);
+    	 int count=0;
+    	 String str="";
+    	 while(s.hasNextLine()) {
+    		 str=s.nextLine();
+    		 count++;
+    	 }
+    	 s.close();
+    	 
+    	 String[] cls= new String[count];
+    	 int[] num= new int[count];
+    	 int c= 0;
+    	 while(sc.hasNextLine()) {
+    		 if(c==count) break;
+    		 cls[c]=sc.next();
+    		 num[c]=sc.nextInt();
+    		 c++;
+    	 }
+    	 sc.close();
+    	 int max=0;
+    	 int maxFrequency=num[0];
+    	 for(int i=0; i<count; i++) {
+    		 if(num[i]>num[max]) {
+    			 max=i;
+    		 }
+    		 if(num[i]>maxFrequency) {
+    			 maxFrequency=num[i];
+    		 }
+    	 }
+    	 
+    	
+    	 
+    	 double prediction=((maxFrequency*100)/k);
+    	 System.out.println(cls[max] + " "+ prediction +"%");
+    	 
+     }
+     
 	public static void main(String[] args) throws FileNotFoundException {
 		//5.2 , 3.1
-		int k = 5;
+		int k = 7;
 		kNN(5.2 , 3.1);
 		double[] sortedDistances= bubbleSort(distances);
 		String[] kData= new String[k];
@@ -102,6 +154,9 @@ public class kNearestNeighbor {
 		
 		
 		frequency(kData);
+		
+		System.out.println("-------------------------");
+		prediction(kData, k);
 		
 	}
 
